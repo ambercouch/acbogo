70d8f0a2459c4b7573cac8885b4cca65d51be84e
Richard Arnold <richard@ambercouch.co.uk>
2021-04-12 17:46:36 +0100

    Shopify setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +14,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware(['auth.shopify'])->name('home');
+
+Route::get('shopify', 'ShopifyController@index')->middleware(['auth.shopify']);
