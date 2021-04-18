a5eb38d1eb5332c48f0591d6b0e5ac153f0825ff
Richie Arnold <richard@ambercouch.co.uk>
2021-04-18 17:12:07 +0100

    Add resources seeds

--- database/seeds/ResourcesSeeder.php
+++ database/seeds/ResourcesSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Resources;
+
 
 class ResourcesSeeder extends Seeder
 {
@@ -11,6 +13,22 @@ class ResourcesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $resources = [
+            'resource1' => [
+                'resource_name' => 'Pages'
+            ],
+            'resource2' => [
+                'resource_name' => 'Products and Variants'
+            ],
+            'resource3' => [
+                'resource_name' => 'Blogs and Posts'
+            ],
+        ];
+
+
+        foreach ($resources as $resource){
+            factory(Resources::class, 1)->create($resource);
+        }
+
     }
 }
