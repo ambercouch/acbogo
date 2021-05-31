7cef12a955f0ecd578525c298421a05a827750c2
Richie Arnold <richard@ambercouch.co.uk>
2021-05-31 13:06:34 +0100

    ACCPF - Add resources and mfg routes

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->middleware(['auth.shopify'])->name('home');
+//List all resources
+Route::get('/', 'resourcesController@index')->middleware(['auth.shopify'])->name('home');
+Route::resource('resources.metafield_groups', MetafieldGroupsController::class)->middleware(['auth.shopify'])->name('*','metafield_groups');
 
 Route::get('shopify', 'ShopifyController@index')->middleware(['auth.shopify']);
 
